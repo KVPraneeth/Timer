78738cf64146e457c2dba0220af7066a006f9926
Praneeth <kvpraneeth007@gmail.com>
2025-03-07 20:18:48 +0530

    Implemented event bubbling

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const currentTime = document.querySelector(".para");
-const startButton = document.querySelector("button[name='start']");
-const stopButton = document.querySelector("button[name='stop']");
-const resetButton = document.querySelector("button[name='reset']");
+const buttonParent = document.querySelector(".btn-container");
+// const startButton = document.querySelector("button[name='start']");
+// const stopButton = document.querySelector("button[name='stop']");
+// const resetButton = document.querySelector("button[name='reset']");
 
 // Variables to store time
 let msec = 0;
@@ -19,27 +20,9 @@ const displayTime = () => {
 
 
 // function to handle start button click
-const handleStartClick = (event) => {
-    if (timerId) {
-        return;
-    }// Prevent multiple intervals from being created
-    timerId = setInterval(() => {
-        msec++;
-        if(msec === 100){
-            msec = 0;
-            seconds++;
-        }
-        if(seconds === 60){
-            seconds = 0;
-            minutes++;
-        }
-        if(minutes === 60){
-            minutes = 0;
-            hours++;
-        }
-        displayTime();
-    }, 10);
-};
+// const handleStartClick = (event) => {
+    
+// };
 /**
  * setInterval() is a JavaScript function that repeatedly executes a specified function at a fixed time interval (in milliseconds).
  * let intervalId = setInterval(callback, delay, arg1, arg2, ...);
@@ -51,25 +34,59 @@ const handleStartClick = (event) => {
 
 
 // function to handle stop button click
-const handleStopClick = (event) => {
-    clearInterval(timerId);
-    timerId = null;
-};// stopping the interval
+// const handleStopClick = (event) => {
+    
+// };// stopping the interval
 
 // function to handle reset button click
-const handleResetClick = (event) => {
-    clearInterval(timerId);
-    timerId = null;
-    msec = 0;
-    seconds = 0;
-    minutes = 0;
-    hours = 0;
-    displayTime();
-};
+// const handleResetClick = (event) => {
+    
+// };
+
+const handleClick = (event) => {
+    const name = event.target.getAttribute("name");
+    if(name === "start"){
+        if (timerId) {
+            return;
+        }// Prevent multiple intervals from being created
+        timerId = setInterval(() => {
+            msec++;
+            if(msec === 100){
+                msec = 0;
+                seconds++;
+            }
+            if(seconds === 60){
+                seconds = 0;
+                minutes++;
+            }
+            if(minutes === 60){
+                minutes = 0;
+                hours++;
+            }
+            displayTime();
+        }, 10);
+    }
+    else if(name === "stop"){
+        clearInterval(timerId);
+        timerId = null;
+    }
+    else if(name === "reset"){
+        clearInterval(timerId);
+        timerId = null;
+        msec = 0;
+        seconds = 0;
+        minutes = 0;
+        hours = 0;
+        displayTime();
+    }
+}
+
 
 // Adding event listeners to buttons
-startButton.addEventListener("click", handleStartClick);
-stopButton.addEventListener("click", handleStopClick)
-resetButton.addEventListener("click", handleResetClick);
+// startButton.addEventListener("click", handleStartClick);
+// stopButton.addEventListener("click", handleStopClick)
+// resetButton.addEventListener("click", handleResetClick);
+
+buttonParent.addEventListener("click", handleClick);
 
 displayTime(); // Display initial time
